eaac266515aee22e9ea4fcd39bd418de340239e7
Licoman <Licomail256@gmail.com>
2023-02-06 11:08:14 +0100

    Add and clean up parameters

--- FireRender.Maya.Src/Context/HybridProContext.cpp
+++ FireRender.Maya.Src/Context/HybridProContext.cpp
@@ -147,26 +147,24 @@ void HybridProContext::setupContextHybridParams(const FireRenderGlobalsData& fir
 		frstatus = rprContextSetParameterByKey1f(frcontext, RPR_CONTEXT_GINI_COEFFICIENT_FOR_GMON, fireRenderGlobalsData.viewportGiniCoeffGmon);
 		checkStatus(frstatus);
 
-		//frstatus = rprContextSetParameterByKey1u(frcontext, RPR_CONTEXT_MATERIAL_CACHE, fireRenderGlobalsData.viewportMaterialCache);
-		//checkStatus(frstatus);
-		//
-		//frstatus = rprContextSetParameterByKey1u(frcontext, RPR_CONTEXT_RESTIR_GI, fireRenderGlobalsData.viewportRestirGI);
-		//checkStatus(frstatus);
-		//
-		//frstatus = rprContextSetParameterByKey1u(frcontext, RPR_CONTEXT_RESTIR_GI_BIAS_CORRECTION, fireRenderGlobalsData.viewportRestirGIBiasCorrection);
-		//checkStatus(frstatus);
-		//
-		//// causes -22 error
-		////frstatus = rprContextSetParameterByKey1f(frcontext, RPR_CONTEXT_RESERVOIR_SAMPLING, fireRenderGlobalsData.viewportReservoirSampling);
-		//checkStatus(frstatus);
-		//
-		//// causes -22 error
-		////frstatus = rprContextSetParameterByKey1f(frcontext, RPR_CONTEXT_RESTIR_SPATIAL_RESAMPLE_ITERATIONS, fireRenderGlobalsData.viewportRestirSpatialResampleIterations);
-		//checkStatus(frstatus);
-		//
-		//// causes -22 error
-		////frstatus = rprContextSetParameterByKey1f(frcontext, RPR_CONTEXT_RESTIR_MAX_RESERVOIRS_PER_CELL, fireRenderGlobalsData.viewportRestirMaxReservoirsPerCell);
-		//checkStatus(frstatus);
+		frstatus = rprContextSetParameterByKey1u(frcontext, RPR_CONTEXT_MATERIAL_CACHE, fireRenderGlobalsData.viewportMaterialCache);
+		checkStatus(frstatus);
+		
+		frstatus = rprContextSetParameterByKey1u(frcontext, RPR_CONTEXT_RESTIR_GI, fireRenderGlobalsData.viewportRestirGI);
+		checkStatus(frstatus);
+		
+		frstatus = rprContextSetParameterByKey1u(frcontext, RPR_CONTEXT_RESTIR_GI_BIAS_CORRECTION, fireRenderGlobalsData.viewportRestirGIBiasCorrection);
+		checkStatus(frstatus);
+		
+		frstatus = rprContextSetParameterByKey1u(frcontext, RPR_CONTEXT_RESERVOIR_SAMPLING, fireRenderGlobalsData.viewportReservoirSampling);
+		checkStatus(frstatus);
+
+		frstatus = rprContextSetParameterByKey1u(frcontext, RPR_CONTEXT_RESTIR_SPATIAL_RESAMPLE_ITERATIONS, fireRenderGlobalsData.viewportRestirSpatialResampleIterations);
+		checkStatus(frstatus);
+
+		frstatus = rprContextSetParameterByKey1u(frcontext, RPR_CONTEXT_RESTIR_MAX_RESERVOIRS_PER_CELL, fireRenderGlobalsData.viewportRestirMaxReservoirsPerCell);
+		checkStatus(frstatus);
+
 	}
 	else
 	{
@@ -176,19 +174,17 @@ void HybridProContext::setupContextHybridParams(const FireRenderGlobalsData& fir
 		frstatus = rprContextSetParameterByKey1f(frcontext, RPR_CONTEXT_GINI_COEFFICIENT_FOR_GMON, fireRenderGlobalsData.productionGiniCoeffGmon);
 		checkStatus(frstatus);
 		
+		frstatus = rprContextSetParameterByKey1u(frcontext, RPR_CONTEXT_PT_DENOISER, fireRenderGlobalsData.productionPtDenoiser);
+		checkStatus(frstatus);
 		
-		//frstatus = rprContextSetParameterByKey1u(frcontext, RPR_CONTEXT_PT_DENOISER, fireRenderGlobalsData.productionPtDenoiser);
-		//checkStatus(frstatus);
-		//
-		//frstatus = rprContextSetParameterByKey1u(frcontext, RPR_CONTEXT_MATERIAL_CACHE, fireRenderGlobalsData.productionMaterialCache);
-		//checkStatus(frstatus);
-		//
-		//frstatus = rprContextSetParameterByKey1u(frcontext, RPR_CONTEXT_RESTIR_GI, fireRenderGlobalsData.productionRestirGI);
-		//checkStatus(frstatus);
-		//
-		//// causes -22 error
-		////frstatus = rprContextSetParameterByKey1f(frcontext, RPR_CONTEXT_RESERVOIR_SAMPLING, fireRenderGlobalsData.productionReservoirSampling);
-		//checkStatus(frstatus);
+		frstatus = rprContextSetParameterByKey1u(frcontext, RPR_CONTEXT_MATERIAL_CACHE, fireRenderGlobalsData.productionMaterialCache);
+		checkStatus(frstatus);
+		
+		frstatus = rprContextSetParameterByKey1u(frcontext, RPR_CONTEXT_RESTIR_GI, fireRenderGlobalsData.productionRestirGI);
+		checkStatus(frstatus);
+		
+		frstatus = rprContextSetParameterByKey1u(frcontext, RPR_CONTEXT_RESERVOIR_SAMPLING, fireRenderGlobalsData.productionReservoirSampling);
+		checkStatus(frstatus);
 	}
 }
 
