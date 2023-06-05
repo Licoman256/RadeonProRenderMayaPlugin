a2df3805858e59ae6088b2bd0bb7be91abf6f9e9
Licoman <Licomail256@gmail.com>
2023-06-05 10:34:06 +0200

    mac fix 4

--- FireRender.Maya.Src/MayaStandardNodesSupport/NodeProcessingUtils.h
+++ FireRender.Maya.Src/MayaStandardNodesSupport/NodeProcessingUtils.h
@@ -103,3 +103,67 @@ void TranslateControlPoints(frw::RampNode& rampNode, const FireMaya::Scope& scop
 
 	rampNode.SetControlPoints(ctrlPointsVals.data(), ctrlPointsVals.size());
 }
+
+// iterate through all control points of the Ramp and save connected nodes data to corresponding control points array entries if such nodes exist
+template <typename RampCtrlPointDataT>
+bool GetConnectedCtrlPointsObjects(MPlug& rampPlug, std::vector<RampCtrlPointDataT>& rampCtrlPoints)
+{
+	MStatus status;
+
+	// ensure valid input
+	bool isArray = rampPlug.isArray(&status);
+	assert(isArray);
+	if (!isArray)
+		return false;
+
+	bool doArraysMatch = rampCtrlPoints.size() == rampPlug.numElements();
+	if (!doArraysMatch)
+	{
+		rampCtrlPoints.pop_back(); // remove "technical" control point created by helper if necessary
+	}
+	doArraysMatch = rampCtrlPoints.size() == rampPlug.numElements();
+	assert(doArraysMatch);
+	if (!doArraysMatch)
+		return false;
+
+	// iterate through control points; passing iterator as a data container here to avoid extra unnecessary data copy
+	auto currCtrlPointIt = rampCtrlPoints.begin();
+	using containerIterT = decltype(currCtrlPointIt);
+	static_assert(std::is_same<containerIterT, typename std::vector<RampCtrlPointDataT>::iterator>::value, "data container type mismatch!");
+
+	bool success = MayaStandardNodeConverters::ForEachPlugInArrayPlug<containerIterT>(rampPlug, currCtrlPointIt, ProcessRampArrayPlugElement);
+	assert(success);
+	assert(currCtrlPointIt == rampCtrlPoints.end());
+
+	// sort values by position (maya returns control points in random order)
+	// we have to do it here since plugs cannot be sorted and if we sort out points before reading plugs, they might become misaligned
+	// instead of alligning them, we sort points only after reading all the plugs
+	std::sort(rampCtrlPoints.begin(), rampCtrlPoints.end(), [](auto first, auto second)->bool {
+		return (first.position < second.position); });
+
+	// add control points to the beginning and the end as copies of neighboring points to remove black edges
+	// needed for rpr ramp internal logic
+	if (rampCtrlPoints.front().position > FLT_EPSILON)
+	{
+		rampCtrlPoints.emplace_back();
+		const auto& first = rampCtrlPoints.begin();
+		auto& ctrlPointRef = rampCtrlPoints.back();
+		ctrlPointRef.ctrlPointData = first->ctrlPointData;
+		ctrlPointRef.method = first->method;
+		ctrlPointRef.position = 0.0f;
+		ctrlPointRef.index = 99; // this value is irrelevant
+		std::rotate(rampCtrlPoints.begin(), rampCtrlPoints.end() - 1, rampCtrlPoints.end());
+	}
+
+	if ((1.0f - rampCtrlPoints.back().position) > FLT_EPSILON)
+	{
+		rampCtrlPoints.emplace_back();
+		const auto& last = rampCtrlPoints.end() - 2;
+		auto& ctrlPointRef = rampCtrlPoints.back();
+		ctrlPointRef.ctrlPointData = last->ctrlPointData;
+		ctrlPointRef.method = last->method;
+		ctrlPointRef.position = 1.0f;
+		ctrlPointRef.index = 100; // this value is irrelevant
+	}
+	return success;
+}
